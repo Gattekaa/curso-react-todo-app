8ead1c568bac687b23a6edec1059a33171b85c59
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-25 12:54:32 -0300

    Done connection with Mongo DB

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,10 @@
-const port = 3003
+const port = 3004
 
-const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-const allowCors = require('./cors')
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(allowCors)
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
