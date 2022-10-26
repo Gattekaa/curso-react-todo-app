209250c132c9b25121e5b991c081078173297e88
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-26 09:02:51 -0300

    Added Menu and Routes components

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,15 +1,7 @@
-import React from 'react'
-import PageHeader from '../template/pageHeader'
+import React from "react";
 
 export default props => (
     <div>
-        <PageHeader name='Sobre' small='Nós'></PageHeader>
-
-        <h2>Nossa História</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
-        <h2>Missão e Visão</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
-        <h2>Imprensa</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
+        <h1>Sobre</h1>
     </div>
 )
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import React from "react";
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
-import Todo from '../todo/todo'
-import About from '../about/about'
+import Todo from "../todo/todo";
+import About from "../about/about";
 
 export default props => (
     <Router history={hashHistory}>
