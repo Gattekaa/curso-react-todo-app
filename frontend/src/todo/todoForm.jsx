94a13e82fe7a8249290a6d4a092e612f9fcd604b
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-26 10:19:01 -0300

    Code refac in Button component and added setState to input text

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,10 @@ import IconButton from "../template/iconButton";
 export default props => (
     <div className="todoForm" role='form'>
         <Grid cols='12 9 10' >
-            <input type="text" id="description" className="form-control" placeholder="Adicione uma tarefa" />
+            <input value={props.description} onChange={props.handleChange} id="description" className="form-control" placeholder="Adicione uma tarefa" />
         </Grid>
         <Grid cols='12 3 2'>
-        <IconButton style='primary' icon='plus'></IconButton>
+        <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
