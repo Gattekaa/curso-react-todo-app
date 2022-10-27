72661f00078f9f6c4d0a2ef22c30d0847a95f3c9
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-27 12:28:46 -0300

    Added Redux to project index

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -7,9 +7,9 @@ export default props => {
     const renderRows = () => {
         const list = props.list || []
         return list.map(todo => (
-            <tr key={todo._id}>
-                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
-                <td>
+            <tr key={todo._id} className="tr-style">
+                <td className={todo.done ? 'markedAsDone' : 'td-wrap'}><span>{todo.description}</span></td>
+                <td className="btn-style">
                     <IconButton style={'success'} icon='check' hide={todo.done} onClick={() => props.handleMarkAsDone(todo)} />
                     <IconButton style={'warning'} icon='undo'  hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)} />
                     <IconButton style='danger' icon='trash-o'  onClick={() => props.handleRemove(todo)} />
